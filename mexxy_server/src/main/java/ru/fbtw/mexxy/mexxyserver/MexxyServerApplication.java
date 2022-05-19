32c537ab19cdee68a29167b01f1e6fb46fcfcfc2
netos23 <nikmoroz2002@outlook.com>
2022-05-19 22:29:30 +0300

    init commit

---
+++ mexxy_server/src/main/java/ru/fbtw/mexxy/mexxyserver/MexxyServerApplication.java
@@ -0,0 +1,13 @@
+package ru.fbtw.mexxy.mexxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MexxyServerApplication.class, args);
+	}
+
+}
---
+++ mexxy_server/src/test/java/ru/fbtw/mexxy/mexxyserver/MexxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fbtw.mexxy.mexxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MexxyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
